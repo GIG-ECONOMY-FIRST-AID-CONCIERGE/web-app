26d769b9ec063686f681c0f40f8933921a5842ce
Felipe Olivares Campos <felipeolivares99@hotmail.com>
2023-05-16 21:17:27 -0300

    starting painel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,16 @@ import styles from "./page.module.css";
 import Header from "@/components/Header";
 
 const Home = (): JSX.Element => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className={styles.ctnOptions}>
+        <div className={styles.optionEnabled}>Atendimento</div>
+        <div className={styles.optionsDisabled}>Histórico</div>
+      </div>
+      <div className={styles.painel}>aaa</div>
+    </>
+  );
 };
 
 export default Home;

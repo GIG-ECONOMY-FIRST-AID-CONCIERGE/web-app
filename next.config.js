ad32c5bc7b8c8c3c0be1b9be2ab6b9e8c70eb18f
Felipe Olivares Campos <felipeolivares99@hotmail.com>
2023-05-19 21:55:18 -0300

    new next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Adicione esta configuração para copiar o arquivo log.js para a pasta de saída durante o processo de construção
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /log\.js$/,
+            loader: 'file-loader',
+            options: {
+                publicPath: `/_next/static/logs/`, // Caminho público para o arquivo log.js
+                outputPath: 'static/logs/', // Caminho de saída relativo dentro da pasta .next
+                name: '[name].[hash].[ext]' // Nome do arquivo de saída
+            }
+        });
 
-module.exports = nextConfig
+        return config;
+    }
+};
+
+module.exports = nextConfig;
+
